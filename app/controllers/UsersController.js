1af2993b8fd5a1f0338ea418f79e839a518ef6c8
peerzadesaif <saif.pirjade@gmail.com>
2020-09-27 13:48:26 +0530

    Created the different controller for meal

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -61,18 +61,7 @@ module.exports.Register = async (req, res) => {
     return res.status(statusTypes.SUCCESS).json(ResponseService.sendResponse(statusTypes.CODE_422, null, null, error));
   }
 };
-module.exports.GetAllMeals = async (req, res) => {
-  return res.json({ status: true, messages: 'GetAllMeals' });
-};
-module.exports.AddMeal = async (req, res) => {
-  return res.json({ status: true, messages: 'AddMeal' });
-};
-module.exports.GetSingleMeal = async (req, res) => {
-  return res.json({ status: true, messages: 'GetSingleMeal' });
-};
-module.exports.UpdateMeal = async (req, res) => {
-  return res.json({ status: true, messages: 'UpdateMeal' });
-};
+
 module.exports.ResetPassword = async (req, res) => {
   try {
     let reqbody = req.body;
