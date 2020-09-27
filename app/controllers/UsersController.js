8ab4663aa02415ad52041e6ecb44a039d662dec6
peerzadesaif <saif.pirjade@gmail.com>
2020-09-27 13:13:53 +0530

    Created the routes and controllers for users and user meals

--- app/controllers/UsersController.js
+++ app/controllers/UsersController.js
@@ -1,7 +1,30 @@
 
 
 
-module.exports.Testing = async (req, res) => {
-  return res.json({ status: true, messages: 'working' });
+module.exports.Login = async (req, res) => {
+  return res.json({ status: true, messages: 'Login' });
+};
+
+module.exports.Register = async (req, res) => {
+  return res.json({ status: true, messages: 'Register' });
+};
+module.exports.GetAllMeals = async (req, res) => {
+  return res.json({ status: true, messages: 'GetAllMeals' });
+};
+module.exports.AddMeal = async (req, res) => {
+  return res.json({ status: true, messages: 'AddMeal' });
+};
+module.exports.GetSingleMeal = async (req, res) => {
+  return res.json({ status: true, messages: 'GetSingleMeal' });
+};
+module.exports.UpdateMeal = async (req, res) => {
+  return res.json({ status: true, messages: 'UpdateMeal' });
+};
+module.exports.ResetPassword = async (req, res) => {
+  return res.json({ status: true, messages: 'ResetPassword' });
+};
+
+module.exports.VerifyUserResetPassword = async (req, res) => {
+  return res.json({ status: true, messages: 'VerifyUserResetPassword' });
 };
 
