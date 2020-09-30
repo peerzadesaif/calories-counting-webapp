66dc961f701470966729e2754f46503c75340b3a
peerzadesaif <saif.pirjade@gmail.com>
2020-09-30 13:13:53 +0530

    Created the routes and controllers for users and user meals

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,7 +2,21 @@ const express = require("express");
 const router = express.Router();
 import UsersController from "../app/controllers/UsersController";
 
-router.route("/user/testing").get(UsersController.Testing);
+router.route("/user/login").get(UsersController.Login);
+
+router.route("/user/register").post(UsersController.Register);
+
+router.route("/user/meals")
+    .get(UsersController.GetAllMeals)
+    .post(UsersController.AddMeal);
+
+router.route("/user/meals/:id")
+    .get(UsersController.GetSingleMeal)
+    .put(UsersController.UpdateMeal);
+
+router.route("/password/reset").post(UsersController.ResetPassword);
+
+router.route("/password/reset/:token").get(UsersController.VerifyUserResetPassword);
 
 
 module.exports = router;
