351c5f0231541a2edac3cc0bdabc95acd4938429
peerzadesaif <saif.pirjade@gmail.com>
2020-10-01 13:28:55 +0530

    Create the services for response, security, logging, response code handler and other related stuff is done as per needed

--- terminate.js
+++ terminate.js
@@ -1,11 +1,12 @@
+import LoggingService from "./app/services/LoggingService";
 
 const terminate = (server, options = { coredump: false, timeout: 500 }) => {
     // Exit Function
     const exit = code => (options.coredump ? process.abort() : process.exit());
     return (code, reason) => (error, promise) => {
-        console.log("SERVER_PROCESS_ERROR", `Process exited with code: ${String(code)}, reason: ${reason}`);
-        if (error && error instanceof Error) console.log("SERVER_PROCESS_ERROR", `Uncaught Exception: ${String(error.message)}`);
-        // if (error && error instanceof Error) console.log(error.message, 'SAIF', error.stack)
+        LoggingService.consoleLog("SERVER_PROCESS_ERROR", `Process exited with code: ${String(code)}, reason: ${reason}`);
+        if (error && error instanceof Error) LoggingService.consoleLog("SERVER_PROCESS_ERROR", `Uncaught Exception: ${String(error.message)}`);
+        // if (error && error instanceof Error) LoggingService.consoleLog(error.message, 'SAIF', error.stack)
         server.close(exit);
         setTimeout(() => exit, options.timeout).unref()
     }
